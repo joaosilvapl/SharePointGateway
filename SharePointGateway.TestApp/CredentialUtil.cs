c050f3465fce3104e9fa7d0efdf5e568b7230354
Joao Silva <jhas@netcompany.com>
2019-05-04 00:37:51 +0200

    Modified TestApp to load config from file and Credential Manager. Added sample config file.

---
+++ SharePointGateway.TestApp/CredentialUtil.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using CredentialManagement;
+
+namespace SharePointGateway.TestApp
+{
+    public static class CredentialUtil
+    {
+        public static NetworkCredential GetCredential(string target, PersistanceType persistanceType)
+        {
+            var cm = new Credential { Target = target, PersistanceType = persistanceType };
+
+            if (!cm.Load())
+            {
+                return null;
+            }
+
+            var networkCredential = new NetworkCredential(cm.Username, cm.Password);
+
+            var userNameParts = networkCredential.UserName.Split('\\');
+            if (userNameParts.Length > 1)
+            {
+                networkCredential.Domain = userNameParts[0];
+                networkCredential.UserName = userNameParts[1];
+            }
+
+            return networkCredential;
+        }
+
+        public static bool SetCredentials(
+            string target, string username, string password, PersistanceType persistenceType)
+        {
+            return new Credential
+            {
+                Target = target,
+                Username = username,
+                Password = password,
+                PersistanceType = persistenceType
+            }.Save();
+        }
+    }
+}
--- SharePointGateway.TestApp/Program.cs
+++ SharePointGateway.TestApp/Program.cs
@@ -1,36 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using CredentialManagement;
 using SharePointGateway.Core;
 
 namespace SharePointGateway.TestApp
 {
     class Program
     {
+        //The credential entry (target) name in the local computer
+        private const string CredentialsEntryName = "SharePointGateway.TestApp.Credentials";
+
+        //Name of config file to be placed in the User folder (c:\users\[userName])
+        private const string ConfigFileName = "SharePointGateway.TestApp.Config.txt";
+
         static void Main()
         {
-            var dataSourceInfo = new DataSourceInfo
-            {
-                SiteUri = "[yoursiteUrl]",
-                RestQueryData = new RestQueryData
-                {
-                    ListTitle = "MyList",
-                    FilterQuery = "ID eq 12833 or ID eq 18912",
-                    SelectQuery = "Id,Title,TimeSpent,Modified,AssignedTo/Name",
-                    OrderBy = "Modified desc",
-                    Expand = "AssignedTo",
-                    MaxResults = 2000,
-                },
-                NetworkCredentials = new NetworkCredential("userName", "password", "domain")
-            };
+            var dataSourceInfo = LoadConfig();
 
             var listItemParser = new ListItemParser();
 
             var listItemRetriever = new ListItemRetriever(new SharePointConnector());
             var items = listItemRetriever.GetListItems(dataSourceInfo, listItemParser).Result;
         }
+
+        public static DataSourceInfo LoadConfig()
+        {
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            var configFilePath = Path.Combine(userFolder, ConfigFileName);
+
+            var lines = File.ReadAllLines(configFilePath);
+
+            var dataSourceInfo = new DataSourceInfo
+            {
+                SiteUri = lines[0],
+                RestQueryData = new RestQueryData
+                {
+                    ListTitle = lines[1],
+                    FilterQuery = lines[2],
+                    SelectQuery = lines[3],
+                    OrderBy = lines[4],
+                    Expand = lines[5],
+                    MaxResults = int.Parse(lines[6])
+                },
+                NetworkCredentials = CredentialUtil.GetCredential(CredentialsEntryName, PersistanceType.LocalComputer)
+            };
+
+            return dataSourceInfo;
+        }
     }
 }
+
